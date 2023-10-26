939f4ecf218e65fa906800d41ed5b9005ba89df6
Alemakave <savercat4@gmail.com>
2023-10-27 00:44:12 +1000

    Реализован тесты для методов add

--- src/test/java/ru/alemakave/hw33/StringListTest.java
+++ src/test/java/ru/alemakave/hw33/StringListTest.java
@@ -27,18 +27,31 @@ public class StringListTest {
 
     @Test
     public void addByIndex() {
-        assertEquals(stringList.add(4, "t"), "t");
-        assertEquals(stringList.add(6, "U"), "U");
-        assertEquals(stringList.add(9, "p"), "p");
-        assertEquals(stringList.add(5, "y"), "y");
-        assertEquals(stringList.add(3, "r"), "r");
-        assertEquals(stringList.add(2, "e"), "e");
-        assertEquals(stringList.add(0, "Q"), "Q");
-        assertEquals(stringList.add(7, "i"), "i");
-        assertEquals(stringList.add(1, "w"), "w");
-        assertEquals(stringList.add(8, "o"), "o");
+        int countItems = 10;
+        for (int i = 0; i < countItems; i++) {
+            stringList.add("z");
+        }
 
-        assertEquals(stringList.toString(), Arrays.toString(new String[]{"Q", "w", "e", "r", "t", "y", "U", "i", "o", "p"}));
+        assertEquals(stringList.add(4, "t"), "t");
+        assertEquals(stringList.toString(), Arrays.toString(new String[]{"z", "z", "z", "z", "t", "z", "z", "z", "z", "z", "z"}));
+        assertEquals(stringList.add(6, "U"), "U");
+        assertEquals(stringList.toString(), Arrays.toString(new String[]{"z", "z", "z", "z", "t", "z", "U", "z", "z", "z", "z", "z"}));
+        assertEquals(stringList.add(9, "p"), "p");
+        assertEquals(stringList.toString(), Arrays.toString(new String[]{"z", "z", "z", "z", "t", "z", "U", "z", "z", "p", "z", "z", "z"}));
+        assertEquals(stringList.add(5, "y"), "y");
+        assertEquals(stringList.toString(), Arrays.toString(new String[]{"z", "z", "z", "z", "t", "y", "z", "U", "z", "z", "p", "z", "z", "z"}));
+        assertEquals(stringList.add(3, "r"), "r");
+        assertEquals(stringList.toString(), Arrays.toString(new String[]{"z", "z", "z", "r", "z", "t", "y", "z", "U", "z", "z", "p", "z", "z", "z"}));
+        assertEquals(stringList.add(2, "e"), "e");
+        assertEquals(stringList.toString(), Arrays.toString(new String[]{"z", "z", "e", "z", "r", "z", "t", "y", "z", "U", "z", "z", "p", "z", "z", "z"}));
+        assertEquals(stringList.add(0, "Q"), "Q");
+        assertEquals(stringList.toString(), Arrays.toString(new String[]{"Q", "z", "z", "e", "z", "r", "z", "t", "y", "z", "U", "z", "z", "p", "z", "z", "z"}));
+        assertEquals(stringList.add(7, "i"), "i");
+        assertEquals(stringList.toString(), Arrays.toString(new String[]{"Q", "z", "z", "e", "z", "r", "z", "i", "t", "y", "z", "U", "z", "z", "p", "z", "z", "z"}));
+        assertEquals(stringList.add(1, "w"), "w");
+        assertEquals(stringList.toString(), Arrays.toString(new String[]{"Q", "w", "z", "z", "e", "z", "r", "z", "i", "t", "y", "z", "U", "z", "z", "p", "z", "z", "z"}));
+        assertEquals(stringList.add(8, "o"), "o");
+        assertEquals(stringList.toString(), Arrays.toString(new String[]{"Q", "w", "z", "z", "e", "z", "r", "z", "o", "i", "t", "y", "z", "U", "z", "z", "p", "z", "z", "z"}));
     }
 
     @Test
